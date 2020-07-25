6a5e2c3d5fed9aa9adf7a69f4571ec59c92177a8
baijunjie <bjjjunjie@qq.com>
2020-07-25 14:58:41 +0800

    aop

---
+++ my.core.mvc/Untility/CustomAutofacAop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Castle.DynamicProxy;
+namespace my.core.mvc.Untility
+{
+    public class CustomAutofacAop:IInterceptor
+    {
+        public CustomAutofacAop()
+        {
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            Console.WriteLine($"执行前{invocation.Method.Name}");
+            invocation.Proceed();
+            Console.WriteLine("执行后");
+        }
+    }
+}
